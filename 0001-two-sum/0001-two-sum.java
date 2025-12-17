15972c5d9a047ab5314dc465c74434f1fe22bef8
London jones <157427796+londonjones4189@users.noreply.github.com>
2025-12-17 13:56:18 -0500

    Time: 45 ms (28.81%), Space: 45.9 MB (75.02%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,15 @@
+/*
+brute force approach: loop through until you find the matching pair
+ */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) { 
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
