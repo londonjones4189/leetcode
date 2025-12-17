6953004178cf45e9c52f21005752de1af29a621d
London jones <157427796+londonjones4189@users.noreply.github.com>
2025-12-17 14:38:11 -0500

    Time: 4 ms (52.56%), Space: 46.4 MB (49.99%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,16 @@
 /*
-brute force approach: loop through until you find the matching pair
+optmized approach: use hashmap for lookup
  */
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) { 
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);                  
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];            
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[]{i, map.get(complement)};
             }
         }
         return new int[]{};
